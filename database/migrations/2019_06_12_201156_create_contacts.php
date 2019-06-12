d8b8fe9a3974cef7c77a172e47c6117199920b8b
LeTaiTu <letaitu1997@gmail.com>
2019-06-12 20:20:58 +0700

    sua 8h

--- database/migrations/2019_06_12_201156_create_contacts.php
+++ database/migrations/2019_06_12_201156_create_contacts.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateSlides extends Migration
+class CreateContacts extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateSlides extends Migration
      */
     public function up()
     {
-        Schema::create('slides', function (Blueprint $table) {
+        Schema::create('contacts', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string("name_slide", 150)->nullable();
-            $table->integer("id_kind")->default(0)->nullable();
-            $table->string('image')->nullable();
+            $table->integer('id_customer');
+            $table->string('text');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateSlides extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('slides');
+        Schema::dropIfExists('contacts');
     }
 }
